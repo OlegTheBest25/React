8789a0c2f12fa0a3b97e0863fcc9aaa15973a013
Buryak Oleg <olburyak@yandex.ru>
2024-04-29 18:54:45 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -1,87 +1,56 @@
-import styles from "./app.module.css";
-import data from "./data.json";
+import "./App.css";
+import style from "./style.module.css";
 import { useState } from "react";
 
 export const App = () => {
-	// Можно задать 2 состояния — steps и activeIndex
-	const [steps, setSteps] = useState(data);
-	const [activeIndex, setActiveIndex] = useState(0);
-
-	// И определить 3 обработчика: Клик назад, Клик вперед, Начать сначала
-	const clickNext = () => {
-		activeIndex === 6 ? setActiveIndex(0) : setActiveIndex(activeIndex + 1);
-	};
-	const clickPreview = () => {
-		console.log(activeIndex);
-		setActiveIndex(activeIndex - 1);
-	};
-
-	const buttonClick = (event) => {
-		setActiveIndex(event.target.innerText - 1);
-	};
-	// И 2 переменных-флага — находимся ли мы на первом шаге, и находимся ли на последнем
-	let isFirst = true;
-	let isEnd = false;
-	if (activeIndex !== 0) {
-		isFirst = false;
-	}
-	if (activeIndex === 6) {
-		isEnd = true;
+	const [value, setValue] = useState("");
+	const [resultColor, setresultColor] = useState(false);
+	function numberClick(item) {
+		setValue(value + item.target.innerText);
 	}
 
+	function operationClick(item) {
+		setresultColor(false);
+		if (item.target.innerText === "c") {
+			setValue("");
+		}
+		if (item.target.innerText === "+") {
+			setresultColor(false);
+			setValue(value + item.target.innerText);
+		}
+		if (item.target.innerText === "-") {
+			setresultColor(false);
+			setValue(value + item.target.innerText);
+		}
+		if (item.target.innerText === "=") {
+			setresultColor(true);
+			setValue(eval(value));
+		}
+	}
+
+	let numberCalc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+	let operetionCalc = ["+", "-", "=", "c"];
 	return (
-		<div className={styles.container}>
-			<div className={styles.card}>
-				<h1>Инструкция по готовке пельменей</h1>
-				<div className={styles.steps}>
-					<div className={styles["steps-content"]}>
-						{data[activeIndex].content}
-					</div>
-					<ul className={styles["steps-list"]}>
-						{steps.map((item, index) => {
-							return (
-								<li
-									className={
-										index === activeIndex
-											? styles["steps-item"] +
-											  " " +
-											  styles.done +
-											  " " +
-											  styles.active
-											: index < activeIndex
-											? styles["steps-item"] +
-											  " " +
-											  styles.done
-											: styles["steps-item"]
-									}
-								>
-									<button
-										className={styles["steps-item-button"]}
-										onClick={buttonClick}
-									>
-										{index + 1}
-									</button>
-									Шаг {index + 1}
-								</li>
-							);
-						})}
-					</ul>
-					<div className={styles["buttons-container"]}>
-						<button
-							className={styles.button}
-							onClick={clickPreview}
-							disabled={isFirst}
-						>
-							Назад
-						</button>
-						<button className={styles.button} onClick={clickNext}>
-							{isEnd ? "Начать сначала" : "Далее"}
-							{/* "Начать сначала", можно сделать этой же кнопкой, просто подменять обработчик и текст в зависимости от условия */}
-							{/* Или заменять всю кнопку в зависимости от условия */}
-						</button>
-					</div>
-				</div>
-			</div>
+		<div className={style.container}>
+			<input
+				className={resultColor ? style.result : style.baseStyle}
+				type="text"
+				value={value}
+			/>
+			{numberCalc.map((item, index) => {
+				return (
+					<button onClick={numberClick} key={item}>
+						{item}
+					</button>
+				);
+			})}
+			{operetionCalc.map((item, index) => {
+				return (
+					<button onClick={operationClick} key={index}>
+						{item}
+					</button>
+				);
+			})}
 		</div>
 	);
 };
